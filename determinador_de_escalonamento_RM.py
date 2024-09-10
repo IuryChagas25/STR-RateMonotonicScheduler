b53afda470279fa7210adf735656873d0256870c
Ikaro Chagas <iurychagas25@gmail.com>
2024-09-10 19:02:36 -0300

    Adição de lista com o escalonamento sugerido e geração de arquivo .JSON com resultado

--- determinador_de_escalonamento_RM.py
+++ determinador_de_escalonamento_RM.py
@@ -1,26 +1,48 @@
 import json
 
+# Abrindo o arquivo "tarefas.json"
 with open("tarefas.json") as tarefas:
     tasks = json.load(tarefas)["tasks"]
 
+# Calculando a utilização total das tarefas
 N = len(tasks)
 U = 0
 for i in range(N):
-    U += tasks[i]['execution_time']/tasks[i]['period']
+    U += tasks[i]['execution_time'] / tasks[i]['period']
 
-cond_suficiente = N*(2**(1/N) - 1)
+# Verifica se o escalonamento é viável
+cond_suficiente = N * (2 ** (1 / N) - 1)
 if U <= cond_suficiente:
-    print('O escalonamento é viável')
+    viabilidade = "viable"
 else:
-    print('O escalonamento não é viável')
+    viabilidade = "not viable"
 
-for k in range(N-1):
-    min = k
-    for j in range(k+1,N):
-        if tasks[min]['period'] > tasks[j]['period']:
-            min = j
-
-    aux = tasks[min]
-    tasks[min] = tasks[k]
+# Ordenação de tarefas pelo período (menor período, maior prioridade) segundo o RM
+for k in range(N - 1):
+    min_idx = k
+    for j in range(k + 1, N):
+        if tasks[min_idx]['period'] > tasks[j]['period']:
+            min_idx = j
+    aux = tasks[min_idx]
+    tasks[min_idx] = tasks[k]
     tasks[k] = aux
-print(tasks)
+
+# Criando  uma lista para o escalonamento sugerido com as prioridades
+sugestao_escalonamento = []
+for idx, task in enumerate(tasks):
+    sugestao_escalonamento.append({
+        "id": task["id"],
+        "priority": idx + 1
+    })
+
+# Criando o formato do arquivo json a ser exportado
+saida = {
+    "schedulability": viabilidade,
+    "suggested_schedule": sugestao_escalonamento
+}
+
+# Exportando o arquivo json
+with open("resultado.json", "w") as arquivo_saida:
+    json.dump(saida, arquivo_saida, indent=4)
+
+print("O resultado foi salvo no arquivo 'resultado.json'.")
